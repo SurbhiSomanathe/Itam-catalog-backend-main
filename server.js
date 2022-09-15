2674137a46a7c6e4ae9514d7bbe60686ebd3d792
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-09-15 18:53:35 +0530

    sever

--- server.js
+++ server.js
@@ -9,18 +9,24 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors({origin:"*"}));
+// app.use(express.json());
+// app.use(cors({origin:"*"}));
 
-app.use("/api/products", productRoutes);
+// app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-// app.use('/api',(request,response) => {
-//     response.send("Hola US")
-// })
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
+// // app.use('/api',(request,response) => {
+// //     response.send("Hola US")
+// // })
+app.get("/", (req, res) => {
+    res.json({ message: "API running..." });
+  });
+  
+  app.use("/api/products", productRoutes);
+  
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
